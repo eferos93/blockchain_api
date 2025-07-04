0dd9b1e5cc45ee9e8d7bebee44b875e03f580fb2
eferos <eros.fabrici@gmail.com>
2025-07-04 12:14:36 +0000

    defined all types for keycloack service

--- keycloak/interface.go
+++ keycloak/interface.go
@@ -14,3 +14,24 @@ type VATokenResponse struct {
 	SessionState    string `json:"sessionState" form:"session_state"`
 	Scope           string `json:"scope" form:"scope"`
 }
+
+type CATokenRequest struct {
+	ClientID           string `json:"clientId" form:"client_id"`
+	ClientSecret       string `json:"clientSecret" form:"client_secret"`
+	GrantType          string `json:"grantType" form:"grant_type"`
+	SubjectToken       string `json:"subjectToken" form:"subject_token"`
+	RequestedTokenType string `json:"requestedTokenType" form:"requested_token_type"`
+}
+
+type UserAttributes struct {
+	GivenName   string `json:"given_name"`
+	FamilyName  string `json:"family_name"`
+	Institution string `json:"institution"`
+	PublicKey   string `json:"pbk"`  //base64
+	Certificate string `json:"pkix"` //base64
+	BcSecret    string `json:"bc_secret"`
+}
+
+type UpdateUserProfileRequest struct {
+	Attributes UserAttributes `json:"attributes"`
+}
